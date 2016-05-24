2fbe3535e5c7fb3b35b190330a0a6e07c65651dc
Sean R H <aun.sswick@gmail.com>
2016-05-23 21:23:17 -0400

    turn off all tomcat error info set to client

--- src/main/java/fpw/SpringBootApp.java
+++ src/main/java/fpw/SpringBootApp.java
@@ -1,7 +1,13 @@
 package fpw;
 
+import org.apache.catalina.core.StandardHost;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -12,5 +18,19 @@ public class SpringBootApp {
 
 	}
 	
+	
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+	    TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory(){
+
+			@Override
+			protected TomcatEmbeddedServletContainer getTomcatEmbeddedServletContainer(Tomcat tomcat) {
+				((StandardHost)tomcat.getHost()).setErrorReportValveClass("");
+				return super.getTomcatEmbeddedServletContainer(tomcat);
+			}
+	    };
+	    return tomcat;
+	}
+
 	
 }
