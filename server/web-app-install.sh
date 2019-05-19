0facd8d5faf1e1bb471fe1b860d1e5f6880ee3bb
Sean R H <aun.sswick@gmail.com>
2019-05-19 19:04:40 -0400

    clean up before publish to resources

--- server/web-app-install.sh
+++ server/web-app-install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 echo STARTING ANGULAR UI WEB APP BUILD AND INSTALL
-cd ../web-app
+cd ..
+rm server/src/main/resources/resources/es2015-polyfills*
+rm server/src/main/resources/resources/favicon.ico
+rm server/src/main/resources/resources/fpw-app-fpw-module*
+rm server/src/main/resources/resources/index.html
+rm server/src/main/resources/resources/main*
+rm server/src/main/resources/resources/polyfills* 
+rm server/src/main/resources/resources/runtime*
+rm server/src/main/resources/resources/styles*
+rm server/src/main/resources/resources/vendor*
+
+cd web-app
 npm install
 ng build --prod --build-optimizer
 cp dist/web-app/* ../server/src/main/resources/resources/
