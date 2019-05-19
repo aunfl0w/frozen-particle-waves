f1f57098ea8842f497685e0766e69c6076bedbe3
Sean R H <aun.sswick@gmail.com>
2019-05-19 18:44:33 -0400

    ignore files

--- server/web-app-install.sh
+++ server/web-app-install.sh
@@ -3,6 +3,6 @@
 echo STARTING ANGULAR UI WEB APP BUILD AND INSTALL
 cd ../web-app
 npm install
-ng build
+ng build --prod --build-optimizer
 cp dist/web-app/* ../server/src/main/resources/resources/
 echo COMPLETED ANGULAR UI WEB APP BUILD AND INSTALL
