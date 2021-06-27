af21f26443f0bbad8c3d165b6d27b10470b2f07c
Sean R H <aun.sswick@gmail.com>
2021-06-27 07:12:51 -0400

    support invalid and self signed certs
    IoS devices ship with wildly broken self signed certs.

--- server/src/main/java/fpw/SpringBootApp.java
+++ server/src/main/java/fpw/SpringBootApp.java
@@ -1,16 +1,58 @@
 package fpw;
 
+import java.security.GeneralSecurityException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class SpringBootApp {
-	
+
 	public static void main(String[] args) {
+		turnOffVerifiedTLS();
 		SpringApplication.run(SpringBootApp.class, args);
 
 	}
-	
-	
+
+	// turn off all ssl validation due to bad cameras TLS implementations
+	public static void turnOffVerifiedTLS() {
+		// Create a trust manager that does not validate certificate chains
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return new X509Certificate[0];
+			}
+			public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {}
+			public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {}
+		} };
+
+
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+
+        // Install the all-trusting host verifier
+        // Install the all-trusting trust manager
+
+        try {    		
+        	HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		} catch (GeneralSecurityException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
