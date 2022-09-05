853df340029a504516099cb14d9b36d2a97dc5b5
Sean R H <aun.sswick@gmail.com>
2022-09-05 12:14:34 -0400

    apache client timeout

--- server/src/main/java/fpw/domain/image/camera/DigestAuthURLCamera.java
+++ server/src/main/java/fpw/domain/image/camera/DigestAuthURLCamera.java
@@ -6,6 +6,7 @@ import java.net.URL;
 
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.protocol.HttpClientContext;
@@ -20,9 +21,15 @@ public class DigestAuthURLCamera extends BasicAuthURLCamera {
 
 	@Override
 	InputStream getInputStream(URL u) throws IOException {
-		
+			final int TIMEOUT = 7000;
 			final CloseableHttpClient httpclient = HttpClients.createDefault();
+			RequestConfig config = RequestConfig.custom()
+					.setConnectTimeout(TIMEOUT)
+					.setConnectionRequestTimeout(TIMEOUT)
+					.setSocketTimeout(TIMEOUT)
+					.build();
 			final HttpClientContext localContext = HttpClientContext.create();
+			localContext.setRequestConfig(config);
 			final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
 			credentialsProvider.setCredentials(new AuthScope(AuthScope.ANY),
 					new UsernamePasswordCredentials(username, password));
